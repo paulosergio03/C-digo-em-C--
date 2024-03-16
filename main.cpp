ea674fdc250436fb444c997685ca97e20306325f
Paulo Sérgio <paulosergioadm9@gmail.com>
2024-03-16 00:53:43 -0300

    Atualização no código

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// inclui biblioteca iostream
+#include <iostream>
+
+// std = standard
+using namespace std;
+//usando o espaço std;
+
+int main() {
+
+//Imprime hello world
+  cout << "Hello World" << endl;
+
+    return 0;
+}
