7d4c8f4d7698c48944b1bd241cd97bcffad47e0b
Paulo Sérgio <paulosergioadm9@gmail.com>
2024-03-18 23:02:28 -0300

    Adionando arquivos

---
+++ ex001.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int A = 30;
+    int B = 10;
+
+    int soma =  A + B;
+    cout << "SOMA = " << soma << endl; 
+
+        return 0;
+}
---
+++ variaveis.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // Verdadeiro ou falso:
+    bool booleano = false;
+    bool Booleano = true;
+
+    // Armazena números interiros:
+    int numero1 = 10;
+    int numero2 = 15;
+
+    // Armazena um Byte:
+    char letra1 = 'M';
+    char letra2 = 'K';
+
+    // Armazena números com uma casa decima: 10.5, 15.10...etc
+    float decimal = 10.5;
+    // Armazema números 2x mais que o Float:
+    double decimal2 = 10.5;
+    // Vai usar muito pouco, quando quiser que não retorne nenhum valor:
+    void exemplo;
+
+        return 0;
+}
